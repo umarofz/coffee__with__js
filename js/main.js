b4e2bd9648510d6af7210d336aa27574d11ab8c7
umarofz <webnajottalim@gmail.com>
2022-06-10 18:01:27 +0500

    FULL COFFEE

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var item__btn = document.querySelector(".plan__r-btn")
+var form = document.querySelector(".plan__right")
+
+form.addEventListener("click", function (evt) {
+  console.log(evt.target);
+})
